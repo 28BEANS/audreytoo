79f4407c8f97317142953b87a72883a181d2bdfe
Vince <vinceevangelista028@gmail.com>
2025-10-19 09:32:59 +0800

    add: dialog message

--- script/nav.js
+++ script/nav.js
@@ -5,3 +5,18 @@ const nav = document.querySelector('nav');
 toggle.addEventListener('click', () => {
     nav.classList.toggle('open');
 });
+
+
+document.getElementById("grow-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // stops the page from reloading
+  alert("Thank you for submitting your plant collection!");
+});
+
+
+const socialLinks = document.querySelectorAll("#social-icon a");
+  socialLinks.forEach(link => {
+    link.addEventListener("click", function(event) {
+      event.preventDefault(); // prevent going to another page
+      alert("This social link is currently unavailable. Stay tuned!");
+    });
+ });
